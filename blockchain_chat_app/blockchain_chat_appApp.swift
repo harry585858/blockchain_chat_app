eb9ce8145c52849cb16494d11588244a4ea379ce
양재민 <yangjaemin585858@gmail.com>
2025-06-17 21:49:03 +0900

    Initial Commit

---
+++ blockchain_chat_app/blockchain_chat_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  blockchain_chat_appApp.swift
+//  blockchain_chat_app
+//
+//  Created by 양재민 on 6/17/25.
+//
+
+import SwiftUI
+
+@main
+struct blockchain_chat_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ blockchain_chat_appTests/blockchain_chat_appTests.swift
@@ -0,0 +1,17 @@
+//
+//  blockchain_chat_appTests.swift
+//  blockchain_chat_appTests
+//
+//  Created by 양재민 on 6/17/25.
+//
+
+import Testing
+@testable import blockchain_chat_app
+
+struct blockchain_chat_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
